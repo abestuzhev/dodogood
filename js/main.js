55f4341af63951ba1881fecc7d81c7a3e2ab6457
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-07-30 09:15:21 +0300

    changes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+    function equalheight(container){
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+        $(container).each(function() {
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0;
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
+    $(window).on('load',function() {
+        equalheight('.c-card-product');
+    });
+    $(window).resize(function(){
+        equalheight('.c-card-product');
+    });
+
+
+    $(document).on('click', function(e){
+        e.preventDefault();
+    })
+})
