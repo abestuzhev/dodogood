56bbe2acd213c8cb1e4033ffae681a79ccaf4c9e
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-08-09 00:20:19 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -57,6 +57,24 @@ $(function(){
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+
+        breakpoints: {
+            // when window width is <= 320px
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is <= 480px
+            730: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is <= 640px
+            980: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
     });
 
     /**/
