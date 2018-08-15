40413d3c4570b7a169c3fb6308f2646170b6f786
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-08-16 01:22:44 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var windowWidth = (window.innerWidth ); // вся ширина окна
+var documentWidth = (document.documentElement.clientWidth ); // ширина минус прокрутка
+
 /*выравнивание ячеек по высоте*/
 function equalheight(container){
     var currentTallest = 0,
@@ -209,6 +212,60 @@ $(function(){
         var $elem_item = $(this).parents('.product-card-size__item');
         $elem_item.remove();
     });
+
+
+    function showPopup(icon, popup) {
+        $(document).on('click', icon, function (e) {
+            var $html = $('html');
+            e.preventDefault();
+            $(popup).addClass('is-visible');
+            $('.mfp-bg').addClass('is-visible');
+
+
+            $html.addClass('lock-html');
+            $('body').addClass('fixed-input');
+            if(windowWidth > documentWidth){
+                $html.css({
+                    'margin-right':'17px'
+                });
+                $('.mfp-wrap').css({
+                    'overflow-y':'scroll'
+                });
+                // console.log('Есть полоса прокрутки');
+            }else {
+                // console.log('Нет полосы прокрутки');
+            }
+        });
+    }
+
+    $(document).on('click', '.popup-close', function (e) {
+        e.preventDefault();
+        var $html = $('html');
+        $(this).parents('.mfp-wrap').removeClass('is-visible');
+        $('.mfp-bg').removeClass('is-visible');
+        $html.css({
+            'margin-right':'0'
+        }).removeClass('lock-html');
+        $('body').removeClass('fixed-input');
+
+
+    });
+
+    $(document).on('click', '.js-popup-close', function (e) {
+        e.preventDefault();
+        var $html = $('html');
+        $(this).parents('.mfp-wrap').removeClass('is-visible');
+        $('.mfp-bg').removeClass('is-visible');
+        $html.css({
+            'margin-right':'0'
+        }).removeClass('lock-html');
+        $('.wrapper').removeClass('fixed-input');
+
+    });
+
+
+    showPopup(".header-login__entry", '.popup-entry');
+    showPopup(".header-login__reg", '.popup-reg');
 
 
 
